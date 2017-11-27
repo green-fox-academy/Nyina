729422ca35c88c7a4366605061923c6004e3b14b
Nyina <dobinyina@gmail.com>
2017-11-27 16:52:27 +0100

    feat: Exercise8 done

---
+++ week-02/day-1/ConsoleApp8/ConsoleApp8/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace GreenFox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int a = 123;
+            int b = 526;
+            var temp = a;
+            a = b;
+            b = temp;
+
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            Console.ReadLine();
+
+        }
+    }
+}
