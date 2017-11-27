7ec775758ac8a7edb507418bc5527f74142aaac3
Nyina <dobinyina@gmail.com>
2017-11-27 17:06:47 +0100

    feat: Exercise10 done

---
+++ week-02/day-1/ConsoleApp10/ConsoleApp10/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace GreenFox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Nyina";
+            int age = 31;
+            double height = 1.69;
+            bool married = true;
+
+            Console.WriteLine(name);
+            Console.WriteLine(age);
+            Console.WriteLine(height);
+            Console.WriteLine(married);
+            Console.ReadLine();
+        }
+    }
+}
