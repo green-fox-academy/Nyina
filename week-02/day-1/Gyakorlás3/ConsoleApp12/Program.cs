041cb25e90f658f7242e2dac99358b71eaa74b05
Nyina <dobinyina@gmail.com>
2017-11-27 23:01:46 +0100

    feat:Gyakorlás1-3 done

---
+++ week-02/day-1/Gyakorlás3/ConsoleApp12/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int even = 0, odd = 0;
+            int[] numbers = new int[] { 0, 1, 2, 3, 4, 5, 6, 7 };
+            foreach (int number in numbers)
+            {
+                if (number % 2 == 0)
+                    even++;
+                else
+                    odd++;
+            }
+            Console.WriteLine("Found {0} Odd Numbers, and {1} Even Numbers.", 
+                odd, even);
+            Console.ReadLine();
+
+        }
+    }
+}
