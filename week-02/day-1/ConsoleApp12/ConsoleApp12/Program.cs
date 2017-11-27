3cabc8f12e8d6c1595e8e2a91670a98bafd0f21e
Nyina <dobinyina@gmail.com>
2017-11-27 23:24:45 +0100

    feat:Exercise14 done

---
+++ week-02/day-1/ConsoleApp12/ConsoleApp12/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                double a = 5;
+                double b = 3;
+                double c = 2;
+                double SurfaceArea = 2 * (a * b + b * c + a * c);
+                double Volume = a * b * c;
+
+                Console.WriteLine(SurfaceArea);
+                Console.WriteLine(Volume);
+                Console.ReadLine();
+
+                // Write a program that stores 3 sides of a cuboid as variables (doubles)
+                // The program should write the surface area and volume of the cuboid like:
+                //
+                // Surface Area: 600
+                // Volume: 1000
+            }
+        }
+    }
+}
---
+++ week-02/day-1/ConsoleApp13/ConsoleApp13/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int currentHours = 14;
+            int currentMinutes = 34;
+            int currentSeconds = 42;
+
+            // Write a program that prints the remaining seconds (as an integer) from a
+            // day if the current time is represented bt the variables
+        }
+    }
+}
