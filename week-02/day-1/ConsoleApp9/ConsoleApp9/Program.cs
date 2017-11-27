66fb553ba2ef1d9a5f360d8b08e2c2dd5d57e6b4
Nyina <dobinyina@gmail.com>
2017-11-27 16:59:52 +0100

    feat: Exercise9

---
+++ week-02/day-1/ConsoleApp9/ConsoleApp9/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace GreenFox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            double massInKg = 81.2;
+            double heightInM = 1.78;
+
+            double dheightInM = heightInM * heightInM;
+            double BMI = massInKg / dheightInM;
+
+            Console.WriteLine(BMI);
+            Console.ReadLine();
+        }
+    }
+}
