a846c620aee6145d19a7e7b3f5ce466221e7714e
Nyina <dobinyina@gmail.com>
2017-11-28 11:31:37 +0100

    feat: Exercise1 modified

--- week-02/day-2/ConsoleApp1/ConsoleApp1/Program.cs
+++ week-02/day-2/ConsoleApp1/ConsoleApp1/Program.cs
@@ -11,14 +11,17 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             int ak = 123;
-            int doubling = ak * 2;
-            Console.WriteLine(doubling);
-         
             
+            Console.WriteLine(doubling(ak));
+          
             // - Create an integer variable named `ak` and assign the value `123` to it
             // - Create a function called `doubling` that doubles it's input parameter
             // - Print the result of `doubling(ak)`
             Console.ReadLine();
+        }
+        public static int doubling(int num1)
+        {
+            return (num1 * 2);
         }
     }
 }
---
+++ week-02/day-2/ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string al = "Greenfox";
+
+            static void 
+            // - Create a string variable named `al` and assign the value `Greenfox` to it
+            // - Create a function called `greet` that greets it's input parameter
+            //     - Greeting is printing e.g. `Greetings dear, Greenfox`
+            // - Greet `al`
+            Console.ReadLine();
+        }
+    }
+}
