d604807ffa4fc637f75da21fc89904efaf00e81e
Nyina <dobinyina@gmail.com>
2017-11-28 17:06:04 +0100

    feat:DoubleItems

---
+++ week-02/day-2/DoubleItems/ConsoleApp10/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] ag = {3, 4, 5, 6, 7};
+
+            for(int i = 0; i < ag.Length; i++)
+            {
+                Console.WriteLine(ag[i] * 2);
+            }
+
+            // - Create an array variable named `ag`
+            //   with the following content: `[3, 4, 5, 6, 7]`
+            // - Double all the values in the array
+            Console.ReadLine();
+        }
+    }
+}
