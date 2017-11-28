eda81f156197a17e16260b3aaffb39a13ce4d99a
Nyina <dobinyina@gmail.com>
2017-11-28 15:58:35 +0100

    feat:Exercise9 done

---
+++ week-02/day-2/ConsoleApp9/ConsoleApp9/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] r= {54, 23, 66, 12};
+            int sum = r[1] + r[2];
+            Console.WriteLine(sum);
+            
+
+            // - Create an array variable named `r`
+            //   with the following content: `[54, 23, 66, 12]`
+            // - Print the sum of the second and the third element
+            Console.ReadLine();
+        }
+    }
+}
