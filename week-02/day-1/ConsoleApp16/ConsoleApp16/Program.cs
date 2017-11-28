d804cc9a62d54fe6c8d6869f8986f41864fa9db2
Nyina <dobinyina@gmail.com>
2017-11-28 09:00:41 +0100

    feat:Exercise16

---
+++ week-02/day-1/ConsoleApp16/ConsoleApp16/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("How many chickens have the farmer?");
+            int chicken = Convert.ToInt32(Console.ReadLine());
+                
+            Console.WriteLine("How many pigs have the farmer?");
+            int pigs = Convert.ToInt32(Console.ReadLine());
+
+            int chickenLegs = chicken * 2;
+            int pigsLegs = pigs * 4;
+
+           Console.WriteLine(chickenLegs);
+           Console.WriteLine(pigsLegs);
+
+            Console.ReadLine();
+
+            // Write a program that asks for two integers
+            // The first represents the number of chickens the farmer has
+            // The seconf represents the number of pigs the farmer has
+            // It should print how many legs all the animals have
+        }
+    }
+}
