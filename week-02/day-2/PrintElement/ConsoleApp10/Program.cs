155092e69639d61b1d5b736dfdf94a4bda3d19de
Nyina <dobinyina@gmail.com>
2017-11-28 16:11:34 +0100

    feat: PrintElement done

---
+++ week-02/day-2/PrintElement/ConsoleApp10/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] af = {4, 5, 6, 7};
+
+            for (int i = 0; i < 4; i++)
+            {
+                Console.WriteLine(af[i]);
+            }
+            // - Create an array variable named `af`
+            //   with the following content: `[4, 5, 6, 7]`
+            // - Print all the elements of `af`
+            Console.ReadLine();
+        }
+    }
+}
