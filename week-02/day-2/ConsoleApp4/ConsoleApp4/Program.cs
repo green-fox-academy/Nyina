7c72bcf7776fcba4a077319b410c7726b996fa80
Nyina <dobinyina@gmail.com>
2017-11-28 12:38:35 +0100

    feat: Exercise4

---
+++ week-02/day-2/ConsoleApp4/ConsoleApp4/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 3, 5, 1, 2, 7 };
+            sum(numbers);
+
+            // - Write a function called `sum` that sum all the numbers
+            //   until the given parameter
+            Console.ReadLine();
+        }
+        static void sum(int[] numbers)
+        {
+            int summary = 0;
+            for (int i = 0; i < numbers.Length; i++)
+                {
+                summary += numbers[i];
+                }
+            Console.WriteLine(summary);
+        }
+    }
+}
