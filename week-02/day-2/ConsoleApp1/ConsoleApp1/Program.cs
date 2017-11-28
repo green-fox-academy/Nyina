9775a118e31a091b7685df16fde782a55a485e0c
Nyina <dobinyina@gmail.com>
2017-11-28 11:17:20 +0100

    feat: Exercise1

---
+++ week-02/day-2/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int ak = 123;
+            int doubling = ak * 2;
+            Console.WriteLine(doubling);
+         
+            
+            // - Create an integer variable named `ak` and assign the value `123` to it
+            // - Create a function called `doubling` that doubles it's input parameter
+            // - Print the result of `doubling(ak)`
+            Console.ReadLine();
+        }
+    }
+}
