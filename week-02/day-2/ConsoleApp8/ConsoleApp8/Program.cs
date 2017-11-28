f2b1bc19604e8db9aedd739dc5c30c406e1393df
Nyina <dobinyina@gmail.com>
2017-11-28 15:49:31 +0100

    feat:Exercise8 done

---
+++ week-02/day-2/ConsoleApp8/ConsoleApp8/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] p1 = {1, 2, 3};
+            int[] p2 = {4, 5};
+
+            if (p2.Length > p1.Length)
+            {
+                Console.WriteLine("p2");
+            }
+            else
+            {
+                Console.WriteLine("p2 is smaller than p1");
+            }
+
+            // - Create an array variable named `p1`
+            //   with the following content: `[1, 2, 3]`
+            // - Create an array variable named `p2`
+            //   with the following content: `[4, 5]`
+            // - Print if `p2` has more elements than `p1`
+            Console.ReadLine();
+        }
+    }
+}
