8fbcfc5256ae5ddd72401f89ef5ae675cf72ec88
Nyina <dobinyina@gmail.com>
2017-11-28 16:59:34 +0100

    feat:Matrix done

---
+++ week-02/day-2/Matrix/ConsoleApp10/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] array = new int [4, 4] 
+            { 
+                {1, 0, 0, 0 },                               
+                {0, 1, 0, 0 },                            
+                {0, 0, 1, 0 },                           
+                {0, 0, 0, 1 } };
+
+            //foreach (int i in array)
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+            Console.WriteLine(array[0,0]);
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                    Console.Write(array[i, j] + " ");
+                Console.WriteLine();
+            }
+
+            // - Create (dynamically) a two dimensional array
+            //   with the following matrix. Use a loop!
+            //
+            //   1 0 0 0
+            //   0 1 0 0
+            //   0 0 1 0
+            //   0 0 0 1
+            //
+            // - Print this two dimensional array to the output
+            Console.ReadLine();
+        }
+    }
+}
