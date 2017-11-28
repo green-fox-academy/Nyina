77c3076dbf60fa520fdc31cfe73eaab75eeb2bcd
Nyina <dobinyina@gmail.com>
2017-11-28 15:38:27 +0100

    feat: exercise7 done

---
+++ week-02/day-2/ConsoleApp7/ConsoleApp7/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] q = {4, 5, 6, 7};
+            Console.WriteLine(q[3]);
+
+            // - Create an array variable named `q`
+            //   with the following content: `[4, 5, 6, 7]`
+            // - Print the third element of `q`
+            Console.ReadLine();
+        }
+    }
+}
