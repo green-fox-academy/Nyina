86144853732513ca19065b321fbebc6f4ff5a1a1
Nyina <dobinyina@gmail.com>
2017-11-28 11:49:28 +0100

    feat: Exercise3 done

---
+++ week-02/day-2/ConsoleApp3/ConsoleApp3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string am = "kuty";
+            appenA(am);
+
+            // - Create a string variable named `am` and assign the value `kuty` to it
+            // - Write a function called `appendA` that gets a string as an input
+            //   and appends an 'a' character to its end
+            // - Print the result of `appendA(am)`
+            Console.ReadLine();
+        }
+        static void appenA(string input)
+        {
+            Console.WriteLine(input + "a");
+        }
+    }
+}
