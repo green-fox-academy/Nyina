0267d6d0fb6d58aa0d28cd1ac41d6a0790826451
Nyina <dobinyina@gmail.com>
2017-11-29 20:37:22 +0100

    feat:ConsoleApp13 done

--- week-02/day-1/ConsoleApp13/ConsoleApp13/Program.cs
+++ week-02/day-1/ConsoleApp13/ConsoleApp13/Program.cs
@@ -14,6 +14,11 @@ namespace ConsoleApp13
             int currentMinutes = 34;
             int currentSeconds = 42;
 
+            int realDay = 24 * 60 * 60;
+            int currentDay = currentHours * 3600 + currentMinutes * 60 + currentSeconds;
+            Console.WriteLine(realDay - currentDay);
+            Console.ReadLine();
+
             // Write a program that prints the remaining seconds (as an integer) from a
             // day if the current time is represented bt the variables
         }
---
+++ week-02/day-1/ConsoleApp17/ConsoleApp17/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace GreenFox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Give me 5 numbers");
+            Console.ReadLine()
+
+            // Write a program that asks for 5 integers in a row,
+            // then it should print the sum and the average of these numbers like:
+            //
+            // Sum: 22, Average: 4.4
+        }
+    }
+}
