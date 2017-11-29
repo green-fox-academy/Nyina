d5e1b2f472412fd627e22d3bcef895fa90a9fa0d
Nyina <dobinyina@gmail.com>
2017-11-29 14:38:18 +0100

    feat:Lists01 done

---
+++ week-02/day-3/Lists01/ConsoleApp1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var planetList = new List<string> { "Mercury", "Venus", "Earth", "Mars", "Jupiter", "Uranus", "Neptune" };
+            planetList.Insert(5, "Saturn");
+            foreach (string planet in planetList)
+
+            // Saturn is missing from the planetList
+            // Insert it into the correct position
+            {
+                Console.WriteLine(planet);
+            }
+            Console.ReadLine();
+        }
+    }
+}
