845ece3d6bd30e39e9638cf92dc9e2897f02088c
Nyina <dobinyina@gmail.com>
2017-11-29 16:26:37 +0100

    feat:Lists04 done

---
+++ week-02/day-3/Lists04/ConsoleApp1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var list = new List<object>();
+            list.Add("Cupcake");
+            list.Add(2);
+            list.Add("Brownie");
+            list.Add(false);
+
+            list[1] = "Croissant";
+            list[3] = "Ice cream";
+            
+            foreach (string cookies in list)
+            {
+                Console.WriteLine(cookies);
+            }
+            Console.Read();
+
+            // Accidentally we added "2" and "false" to the list. 
+            // Your task is to change from "2" to "Croissant" and change from "false" to "Ice cream"
+            // No, don't just remove the lines
+
+            Console.WriteLine(list);
+        }
+    }
+}
