de0b98ba807ce720165c8400a1d9813e1968a2d2
Nyina <dobinyina@gmail.com>
2017-11-29 17:14:05 +0100

    feat:Lists06 done

---
+++ week-02/day-3/Lists06/ConsoleApp1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace IsInList
+{
+    public class IsInList
+    {
+        public static void Main(string[] args)
+        {
+            var list = new List<int> { 2, 4, 6, 8, 10, 12, 14, 16 };
+
+            var other = new List<int> { 4, 8, 12, 16 };
+            //if list.Equals(other, StringComparison.CurrentCultureIgnoreCase))
+
+            if (list.Equals(other))
+            {
+                Console.WriteLine("TRUE");
+            }
+            else
+            {
+                Console.WriteLine("FALSE");
+            }
+
+            Console.ReadLine();
+                
+            // Check if list contains all of the following elements: 4,8,12,16
+            // Print "true" if it contains all, otherwise print "false"
+            // Can you do both the different approaches you tried in the previous one?
+
+        }
+    }
+}
