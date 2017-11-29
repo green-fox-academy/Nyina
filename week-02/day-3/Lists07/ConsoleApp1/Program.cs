b8f831a9da40beeb3f59626c0fab376e5bbb2f07
Nyina <dobinyina@gmail.com>
2017-11-29 17:29:24 +0100

    feat:Lists07 done

---
+++ week-02/day-3/Lists07/ConsoleApp1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace QuoteSwap
+{
+    public class QuoteSwap
+    {
+        public static void Main(string[] args)
+        {
+            var list = new List<string> { "What", "I", "do", "create,", "I", "cannot", "not", "understand." };
+            list[2] = "cannot";
+            list[5] = "do";
+
+            foreach (string quote in list)
+            {
+                Console.Write(quote + " ");
+            }
+            Console.Read();
+
+            //"What I cannot create, I do not understand" ?
+
+            // Accidentally I messed up this quote from Richard Feynman.
+            // Two words are out of place
+            // Your task is to fix it by swapping the right words with code
+
+            // Also, print the sentence to the output with spaces in between.
+
+        }
+    }
+}
