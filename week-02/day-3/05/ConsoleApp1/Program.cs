b75e357dc5b8532181b1380ee125afa17e6b0616
Nyina <dobinyina@gmail.com>
2017-11-29 14:23:00 +0100

    feat:05 done

---
+++ week-02/day-3/05/ConsoleApp1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string reversed = ".eslaf eb t'ndluow ecnetnes siht ,dehctiws erew eslaf dna eurt fo sgninaem eht fI";
+            char[] inputreversed = reversed.ToCharArray();
+            Array.Reverse(inputreversed);
+            string output = new string(inputreversed);
+
+            // Create a function that can reverse a String, which is passed as the parameter
+            // Use it on this reversed string to check it!
+
+            Console.WriteLine(output);
+            Console.ReadLine();
+        }
+    }
+}
