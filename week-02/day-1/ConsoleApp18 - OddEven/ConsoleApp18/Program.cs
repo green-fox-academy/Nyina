7a925b373257a5fd317aad50c3568c06c044d351
Nyina <dobinyina@gmail.com>
2017-11-30 11:05:00 +0100

    feat:ConsoleApp18 - OddEven / 1-day done

---
+++ week-02/day-1/ConsoleApp18 - OddEven/ConsoleApp18/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace GreenFox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Give me a number!");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            if (number % 2 == 0)
+                {
+                Console.WriteLine("Even");
+            }
+            else
+            {
+                Console.WriteLine("Odd");
+            }
+
+            Console.ReadLine();
+
+            // Write a program that reads a number from the standard input,
+            // Then prints "Odd" if the number is odd, or "Even" if it is even.
+
+        }
+    }
+}
