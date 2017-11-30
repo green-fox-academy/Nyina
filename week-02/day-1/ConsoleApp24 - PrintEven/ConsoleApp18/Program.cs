ee5ad0629f63e6428a5a472324ff5398b9ef64fb
Nyina <dobinyina@gmail.com>
2017-11-30 12:37:53 +0100

    feat:ConsoleApp24 - PrintEven

---
+++ week-02/day-1/ConsoleApp24 - PrintEven/ConsoleApp18/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace GreenFox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            for (int i = 0; i <= 500; i = i+2)
+            {
+                Console.WriteLine(i);
+            }
+            Console.ReadLine();
+
+            // Create a program that prints all the even numbers between 0 and 500
+        }
+    }
+}
