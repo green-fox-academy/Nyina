142cb74b19604a05d5a498a5083fe175b8341f11
Nyina <dobinyina@gmail.com>
2017-11-30 11:20:51 +0100

    feat:ConsoleApp20 - PrintBigger / day-01

---
+++ week-02/day-1/ConsoleApp20 - PrintBigger/ConsoleApp18/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace GreenFox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Give me 2 numbers!");
+            int number1 = Convert.ToInt32(Console.ReadLine());
+            int number2 = Convert.ToInt32(Console.ReadLine());
+
+            if (number1 < number2)
+            {
+                Console.WriteLine(number2);
+            }
+            else
+            {
+                Console.WriteLine(number1);
+            }
+            Console.ReadLine();
+
+            // Write a program that asks for two numbers and prints the bigger one
+        }
+    }
+}
