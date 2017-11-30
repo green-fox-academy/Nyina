3032f60d6e75e94867623b18ce657158efb39cb2
Nyina <dobinyina@gmail.com>
2017-11-30 12:27:50 +0100

    feat:ConsoleApp23 - IWontCheatOnExams

---
+++ week-02/day-1/ConsoleApp23 - IWontCheatOnTheExams/ConsoleApp18/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace GreenFox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 99; i++)
+            {
+                Console.WriteLine("I won't cheat on the exam!");
+            }
+            Console.ReadLine();
+            // Create a program that writes this line 100 times:
+            // "I won't cheat on the exam!"
+        }
+    }
+}
