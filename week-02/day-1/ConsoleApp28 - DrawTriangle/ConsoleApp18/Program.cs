8a13e413c13eaf0089383cdec2bec8dc534cb3f3
Nyina <dobinyina@gmail.com>
2017-11-30 17:38:06 +0100

    feat:ConsoleApp29 - DrawPyramid is not finished

---
+++ week-02/day-1/ConsoleApp28 - DrawTriangle/ConsoleApp18/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace GreenFox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Give me a number!");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            string star = "*";
+            for(int i = 0; i <= number; i++)
+            {
+               Console.WriteLine(star);
+                star += "*";
+            }
+            Console.ReadLine();
+
+            // Write a program that reads a number from the standard input, then draws a
+            // triangle like this:
+            //
+            // *
+            // **
+            // ***
+            // ****
+            //
+            // The triangle should have as many lines as the number was
+        }
+    }
+}
---
+++ week-02/day-1/ConsoleApp29 - DrawPyramid/ConsoleApp18/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace GreenFox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Give me one number");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            string star = "*";
+            string space = " ";
+
+            for (int circle = 0; circle < number/2; circle++)
+            {
+                for (int i = 1; i < number; i = i + 2)
+                {
+                    for (int j = number - i; j >= 0; j--)
+                    {
+                        Console.Write(space);
+                        //space.Substring(0);
+                    }
+                    Console.Write(star);
+                    //star += "*";  
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+
+            // Write a program that reads a number from the standard input, then draws a
+            // pyramid like this:
+            //
+            //
+            //    *
+            //   ***
+            //  *****
+            // *******
+            //
+            // The pyramid should have as many lines as the number was
+        }
+    }
+}
