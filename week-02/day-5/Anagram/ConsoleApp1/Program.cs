bcec3558d7ce249e8b572bf1dabd797db525e357
Nyina <dobinyina@gmail.com>
2017-12-01 13:55:36 +0100

    feat: Guess my number

---
+++ week-02/day-5/Anagram/ConsoleApp1/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Give me 2 words!");
+            string word1 = Console.ReadLine();
+            string word2 = Console.ReadLine();
+            Anagram(word1, word2);
+            Console.ReadLine();
+        }
+
+        static void Anagram(string string1, string string2)
+        {
+
+            List<string> stringOne = new List<string>() {string1};
+            List<string> stringTwo = new List<string>() {string2};
+
+            int length1 = string1.Length;
+            int length2 = string2.Length;
+            bool answer = true;
+            int character = 0;
+
+            if (length1 == length2)
+            {
+                for (int i = 0; i < length1; i++){
+                    if (stringOne.Remove(stringTwo[i]))
+                    {
+                        character++;
+                    } 
+                }
+                if (character == string1.Length)
+                {
+                    answer = true;
+                }
+                else
+                {
+                    answer = false;
+                }
+            }
+            Console.WriteLine(answer);
+        }
+    }
+
+}
---
+++ week-02/day-5/Guess my number/ConsoleApp1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("I guessed a number. Find out which was it! You have 5 lives.");
+            int guessedNumber = Convert.ToInt32(Console.ReadLine());
+            
+            int myNumber = 32;
+            int lives = 5;
+
+            for (lives = 4; lives > 0; lives--)
+            {
+                if (guessedNumber > 32)
+                {
+                    Console.WriteLine("Too high. You have {0} lives left.", lives);
+                }
+                if (guessedNumber < 32)
+                {
+                    Console.WriteLine("Too low. You have {0} lives left.", lives);
+                }
+                if (myNumber == guessedNumber)
+                {
+                    Console.WriteLine("Congratulations. You won!", lives);
+                    break;
+                }
+            guessedNumber = Convert.ToInt32(Console.ReadLine());
+
+            }
+            Console.WriteLine("GAME OVER");
+            Console.ReadLine();
+        }
+    }
+}
