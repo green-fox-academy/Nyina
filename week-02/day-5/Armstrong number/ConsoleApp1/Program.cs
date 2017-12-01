a39a9b18e5b2a6ecc333925bc85d0f277ca65bac
Nyina <dobinyina@gmail.com>
2017-12-01 15:43:07 +0100

    feat: Anagram & Armstrong number are in progress

---
+++ week-02/day-5/Armstrong number/ConsoleApp1/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Give me a number! Let's check together wether it is an Armstrong number!");
+            int number = Convert.ToInt32(Console.ReadLine());
+            
+            //int numOfDigits = input.Count(char.IsDigit);
+
+            double result = 0;
+            int numberLength = Length(number);
+            
+
+            //char.GetNumericValue(number);
+            //var digits = SeparateTheDigits(number);
+            //var numberList = new int[] { number };
+
+            var digits = number.ToString();
+            var numberArray = digits.ToString();
+
+            for (int i = 0; i <= numberLength; i++ )
+            {
+                Convert.ToDouble(number);
+                Convert.ToDouble(numberLength);
+                result += Math.Pow(numberArray[i], numberLength);
+            }
+            Convert.ToInt32(result);
+            if (result == number)
+                {
+                Console.WriteLine("WOW, You have an Armstorng number!");
+                }
+            else
+                {
+                Console.WriteLine("Keep trying!");
+                }
+            Console.ReadLine();
+
+        }
+        public static int Length(int number)
+        {
+            number = Math.Abs(number);
+            int length = 1;
+            while ((number /= 10) >= 1)
+                length++;
+            return length;
+        }
+    }
+}
