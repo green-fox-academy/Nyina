3b8d60c83aea6f1b4322af56752ab7c926a939c9
Nyina <dobinyina@gmail.com>
2017-12-02 17:50:43 +0100

    feat: Reverse_list

---
+++ week-02/day-2/Reverse_list/ConsoleApp12/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Reverse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] aj = { 3, 4, 5, 6, 7 };
+
+
+            for (int i = aj.Length-1 ; i >= 0; i--)
+            {
+                Console.Write(aj[i]);
+            }
+            
+            Console.ReadKey();
+
+            // - Create an array variable named `aj`
+            //   with the following content: `[3, 4, 5, 6, 7]`
+            // - Reverse the order of the elements in `aj`
+            // - Print the elements of the reversed `aj`
+            Console.ReadLine();
+        }
+    }
+}
