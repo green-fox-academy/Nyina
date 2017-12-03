8f7ea78b9b836cd5e6575e20cc590ab92898cbea
Nyina <dobinyina@gmail.com>
2017-12-03 13:50:22 +0100

    feat:StreamWriter done

---
+++ week-02/StreamWriter/ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] lines = { "This is now a new information", "This is the second", "This is the last" };
+            using (System.IO.StreamWriter writer = new System.IO.StreamWriter("SomeNewText.txt"))
+            foreach(var singleLine in lines)
+            {
+                Console.WriteLine(singleLine);
+            }
+            Console.ReadLine();
+        }
+    }
+}
