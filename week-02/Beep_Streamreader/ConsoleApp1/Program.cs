30d5a83f4b9692b2e2342a49e06fb1070a88671d
Nyina <dobinyina@gmail.com>
2017-12-03 20:40:46 +0100

    feat: DrawSquare done

---
+++ week-02/Beep_Streamreader/ConsoleApp1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string[] lines = System.IO.File.ReadAllLines("Value.txt");
+            foreach (var line in lines)
+            {
+                Console.WriteLine(line);
+                /*string[] values = line.Split(',');
+                Int64 frequency = Convert.ToInt64(values[0]);
+                Int64 duration = Convert.ToInt64(values[1]);
+                Console.Beep(frequency, duration);*/
+                
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ week-02/day-1/ConsoleApp31 - DrawSquare/ConsoleApp1/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace GreenFox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Give me a number!");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 1; i <= number; i++)
+            {
+                if (i == 1)
+                {
+                    for (int j = 1; j <=number; j++ )
+                    Console.Write("*");
+                }
+
+                if (i>1 && i < number)
+                {
+                    for (int k = 1;  k <= number; k++)
+                    {
+                        if (k == 1 || k == number)
+                        {
+                            Console.Write("*");
+                        }
+                        else
+                        {
+                            Console.Write(" ");
+                        }
+                    }
+                }
+
+
+                if (i == number)
+                {
+                    for (int m = 1; m <= number; m++)
+                    Console.Write("*");
+                }
+
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+
+            // Write a program that reads a number from the standard input, then draws a
+            // square like this:
+            //
+            //
+            // %%%%%
+            // %   %
+            // %   %
+            // %   %
+            // %   %
+            // %%%%%
+            //
+            // The square should have as many lines as the number was
+        }
+    }
+}
