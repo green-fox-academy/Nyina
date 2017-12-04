15852aadd5e977c1f6f890014cbf1bec79c48683
Nyina <dobinyina@gmail.com>
2017-12-04 22:06:32 +0100

    feat: Doubled in progress

---
+++ week-03/day1/Doubled/ConsoleApp1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            string[] DuplText = File.ReadAllLines("duplicated-chars.txt");
+
+            string[] OkText = RepeatedLetters(DuplText);
+            File.WriteAllLines("OkTxt.txt", OkText);
+
+        }
+
+        static string RepeatedLetters(string[] word)
+        {
+             string[] temp = "";
+             for (int i = 0; i < word.Count() - 1; i++)
+             {
+                
+                 if (word[i] == word[i + 1])
+                 {
+                    temp = word[i];
+                 }
+             }
+             return word;
+           
+        }
+
+    }
+}
