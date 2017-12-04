1bc743dfb2092a88dec9788ec967a5683d80c3e1
Nyina <dobinyina@gmail.com>
2017-12-04 12:03:42 +0100

    feat: 01DivideByZero done

---
+++ week-03/day1/01DivideByZero/ConsoleApp1/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Give me a number!");
+            int num = int.Parse(Console.ReadLine());
+            Divider(num);
+
+
+            // create a function that takes a number
+            // divides ten with it,
+            // and prints the result.
+            // it should print "fail" if the parameter is 0
+        }
+        static void Divider(int number)
+        {
+            try
+            {
+                int result = (10 / number);
+                Console.WriteLine(result);
+            }
+            catch
+            {
+                Console.WriteLine("fail");
+            }
+            /*VAGY
+            catch (DivideByZeroException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            */
+            finally
+            {
+                Console.ReadLine();
+            }
+        }
+
+    }
+}
