ffd6afd06f4abee80ca84215f94031bb6fb8a9a3
Nyina <dobinyina@gmail.com>
2017-12-04 21:03:58 +0100

    feat: CopyFile done

--- week-03/day1/CopyFile/ConsoleApp1/Program.cs
+++ week-03/day1/CopyFile/ConsoleApp1/Program.cs
@@ -11,43 +11,25 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            CopyFile("NewFile.txt");
+            CopyFile("TextFile1.txt", "NewFile.txt");
 
             // Write a function that reads all lines of a file and writes the read lines to an other file (a.k.a copies the file)
             // It should take the filenames as parameters
             // It should return a boolean that shows if the copy was successful
         }
 
-        static void CopyFile(string filename)
+        static bool CopyFile(string filename, string goalFile)
         {
             //Console.WriteLine("Give me a path");
             //filename = Console.ReadLine();
-            bool crash = false;
-            bool success = true;
 
-            string[] lines = File.ReadAllLines(filename);
-            try
-            {
-                foreach (var line in lines)
-                {
-                    Console.WriteLine(line);
-                }
-                string[] lines = { "This is now a new information", "This is the second", "This is the last" };
-                StreamWriter sw = new StreamWriter("NewFile", true);
-                sw.WriteLine(line1);
-                sw.Close();
-            }
-            catch (Exception)
-            {
-                Console.WriteLine(crash);
-            }
-
-            finally
-            {
-                Console.WriteLine(success);
+                string[] lines = File.ReadAllLines(filename);
+                File.WriteAllLines(goalFile, lines);
                 Console.ReadLine();
-            }
+                return true;
 
+                /*Console.ReadLine();
+                return false;*/
 
         }
 
