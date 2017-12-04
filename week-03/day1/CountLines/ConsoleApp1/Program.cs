be2869f9bbdb2541524db49b61a4d5a9521def5b
Nyina <dobinyina@gmail.com>
2017-12-04 13:51:15 +0100

    feat: CountLines

---
+++ week-03/day1/CountLines/ConsoleApp1/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            NumberOfLines("TextFile1.txt");
+
+            // Write a function that takes a filename as string,
+            // then returns the number of lines the file contains.
+            // It should return zero if it can't open the file, and
+            // should not raise any error.
+        }
+        static void NumberOfLines(string filename)
+        {
+            string[] array = { };
+
+            try
+            {
+                array = File.ReadAllLines(filename, Encoding.Default);
+            }
+            catch
+            {
+                Console.WriteLine("0");
+            }
+            finally
+            {
+                foreach (var file in array)
+                {
+                    Console.WriteLine(file);
+                }
+
+                int lines = array.Length;
+
+                Console.WriteLine(lines);
+                Console.ReadLine();
+            }
+            
+
+        }
+    }
+}
