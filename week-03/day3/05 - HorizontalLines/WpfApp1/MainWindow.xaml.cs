44ad0d716d96fd077aea2c1e0df65984b71c2331
Nyina <dobinyina@gmail.com>
2017-12-06 13:44:44 +0100

    feat: 05 - HorizontalLinesV2 done

--- week-03/day3/05 - HorizontalLines/WpfApp1/MainWindow.xaml.cs
+++ week-03/day3/05 - HorizontalLines/WpfApp1/MainWindow.xaml.cs
@@ -22,7 +22,13 @@ namespace HorizontalLines
         {
             InitializeComponent();
             var foxDraw = new FoxDraw(canvas);
-            DrawGreenLines(foxDraw);
+
+            double ystart = 150;
+            double yend = 150;
+            DrawGreenLines(foxDraw, ystart, yend);
+
+
+
             // create a line drawing function that takes 2 parameters:
             // the x and y coordinates of the line's starting point
             // and draws a 50 long horizontal line from that point.
@@ -30,14 +36,19 @@ namespace HorizontalLines
 
         }
 
-        public static void DrawGreenLines(FoxDraw foxDraw)
+        public static void DrawGreenLines(FoxDraw foxDraw, double y1, double y2)
         {
-            foxDraw.StrokeColor(Colors.Green);
-            foxDraw.DrawLine(150, 210, 200, 210);
 
-            var startPoint = new Point(0, 20);
-            var endPoint = new Point(100, 20);
-            foxDraw.DrawLine(startPoint, endPoint);
+
+            foxDraw.StrokeColor(Colors.Green);
+
+            for (int i = 0; i < 3; i++)
+            {
+                foxDraw.DrawLine(150, y1, 280, y2 );
+                y1 = y1 + 50;
+                y2 = y2 + 50;
+            }
+
         }
     }
 }
