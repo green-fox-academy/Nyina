4b71201a5e68712b25a1df549011c1b67d01aeca
Nyina <dobinyina@gmail.com>
2017-12-06 15:02:31 +0100

    feat: 08 - PositionSquare

---
+++ week-03/07 - FourRectangles/WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using GreenFox;
+
+namespace FourRectangles
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            var foxDraw = new FoxDraw(canvas);
+            DrawRectangle(foxDraw);
+            // draw four different size and color rectangles.
+
+
+
+        }
+        public static void DrawRectangle(FoxDraw foxDraw)
+        {
+            double xstart = 10;
+            double ystart = 10;
+            double with = 80;
+            double heigh = 50;
+            string[] color = { Red, Green, Yellow, Black };
+
+            for (int i = 0; i < 5; i++)
+            {
+                xstart = xstart + 80;
+                ystart = ystart + 100;
+                with = with + 50;
+                heigh = heigh + 90;
+                foxDraw.DrawRectangle(xstart, ystart, with, heigh);
+                foxDraw.FillColor(Colors.color);
+            }
+        }
+    }
+}
---
+++ week-03/08 - PositionSquare/WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using GreenFox;
+
+namespace PositionSquare
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            var foxDraw = new FoxDraw(canvas);
+
+            double x1 = 10;
+            double y1 = 10;
+
+            DrawRectangle(foxDraw, x1, y1);
+
+            // create a square drawing function that takes 2 parameters:
+            // the x and y coordinates of the square's top left corner
+            // and draws a 50x50 square from that point.
+            // draw 3 squares with that function.            
+
+        }
+        public static void DrawRectangle(FoxDraw foxDraw, double xstart, double ystart)
+        {
+            
+            Random randomColor = new Random();
+
+            for (int i = 0; i < 3; i++)
+            {
+                xstart = xstart + 80;
+                ystart = ystart + 80;
+                foxDraw.DrawRectangle(xstart, ystart, 50, 50);
+                foxDraw.FillColor(Color.FromRgb((byte)randomColor.Next(), (byte)randomColor.Next(), (byte)randomColor.Next()));
+
+            }
+        }
+    }
+}
