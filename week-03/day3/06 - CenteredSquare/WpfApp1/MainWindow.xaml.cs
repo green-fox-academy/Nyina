b9b19a7e447834d556bdbabdb67b057fd055ecd6
Nyina <dobinyina@gmail.com>
2017-12-06 12:30:16 +0100

    feat: 06 - CenteredSquare

---
+++ week-03/day3/06 - CenteredSquare/WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using GreenFox;
+
+namespace CenteredSquare
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            var foxDraw = new FoxDraw(canvas);
+            DrawGreenPolygon(foxDraw);
+            // draw a green 10x10 square to the canvas' center.
+
+        }
+        public static void DrawGreenPolygon(FoxDraw foxDraw)
+        {
+            var points = new List<Point>();
+            points.Add(new Point(100, 160));
+            points.Add(new Point(150, 160));
+            points.Add(new Point(150, 210));
+            points.Add(new Point(100, 210));
+
+            foxDraw.FillColor(Colors.Green);
+            foxDraw.DrawPolygon(points);
+        }
+    }
+}
