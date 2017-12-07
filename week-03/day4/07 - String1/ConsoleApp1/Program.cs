db7555714bb2dffa892f7c8c568e294515a925d7
Nyina <dobinyina@gmail.com>
2017-12-07 15:56:59 +0100

    feat: 07 - String1 in progress

---
+++ week-03/day4/07 - String1/ConsoleApp1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(changeY("xellow"));
+            Console.ReadLine();
+
+            // Given a string, compute recursively (no loops) a new string where all the
+            // lowercase 'x' chars have been changed to 'y' chars.
+        }
+
+        public static string changeY(string text)
+        {
+            int n = text.Length;
+
+
+            if (text.IndexOf("x") == -1)
+            {
+                return text;
+            }
+            else
+            {
+                Console.WriteLine(changeY(text.IndexOf("x").Replace("x","y"));
+
+                //Console.WriteLine(text.Replace(changeY(text.IndexOf("x"), "y");
+            }
+        }
+    }
+}
+
