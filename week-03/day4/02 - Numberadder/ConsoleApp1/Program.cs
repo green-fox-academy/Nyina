62cae0984e0e1480acb5bfafecf2b74f5c774007
Nyina <dobinyina@gmail.com>
2017-12-07 13:06:04 +0100

    feat: 02 - Numberadder

---
+++ week-03/day4/02 - Numberadder/ConsoleApp1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(AddRecursion(3));
+            Console.ReadLine();
+
+            // Write a recursive function that takes one parameter: n and adds numbers from 1 to n.
+        }
+        public static int AddRecursion(int number)
+        {
+            if (number == 1)
+            {
+                return 1;
+            }
+            else
+            {
+               return number + AddRecursion(number - 1);
+            }
+
+        }
+    }
+}
