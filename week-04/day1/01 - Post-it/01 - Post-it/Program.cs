9763b23a46b2930df1358f919066e854ff36a9e0
Nyina <dobinyina@gmail.com>
2017-12-11 13:14:53 +0100

    feat: 01 - Post it

---
+++ week-04/day1/01 - Post-it/01 - Post-it/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using _01___Post_it;
+
+namespace _01___Post_it
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var postItList = new PostIt();
+            postItList.TextColor = "blue";
+            postItList.BackgroundColor = "orange";
+            postItList.Text = "Idea 1";
+
+            Console.WriteLine(postItList.TextColor);
+            Console.WriteLine(postItList.BackgroundColor);
+            Console.WriteLine(postItList.Text);
+            Console.ReadLine();
+        }
+    }
+}
