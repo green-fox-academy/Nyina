3ed0766d1c2273d91df91000da07b6ca595b50e1
Nyina <dobinyina@gmail.com>
2017-12-11 22:37:37 +0100

    feat: Animal dona

---
+++ week-04/day1/Animal/Animal/Animal.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Animal
+{
+    class Animal
+    {
+        public int Hunger;
+        public int Thirst;
+
+        public Animal()
+        {
+            Hunger = 50;
+            Thirst = 50;
+        }
+
+        public void Eat()
+        {
+            Hunger--;
+        }
+
+        public void Drink()
+        {
+            Thirst--;
+        }
+
+        public void Play()
+        {
+            Hunger++;
+            Thirst++;
+        }
+    }
+}
---
+++ week-04/day1/Animal/Animal/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Animal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal animal = new Animal();
+
+            Console.WriteLine(animal.Hunger);
+            Console.WriteLine(animal.Thirst);
+            animal.Eat();
+            Console.WriteLine(animal.Hunger);
+            animal.Drink();
+            Console.WriteLine(animal.Thirst);
+            animal.Play();
+            Console.WriteLine(animal.Hunger);
+            Console.WriteLine(animal.Thirst);
+
+
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
