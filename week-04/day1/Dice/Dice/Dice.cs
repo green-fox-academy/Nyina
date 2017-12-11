76462ece8acd061115b3b33e8b3bba3ae438c75a
Nyina <dobinyina@gmail.com>
2017-12-11 16:40:33 +0100

    feat: Dice - Patrik explanation

---
+++ week-04/day1/Dice/Dice/Dice.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dice
+{
+    class Dice
+    {
+        //a dobás eltárolására hozzuk létre:
+        public int D6 { get; set; }
+        public int reD6 { get; set; }
+        public int Current { get; set; }
+
+        public static Random random = new Random();
+
+        //a constructornak ugyanaz a neve, mint a classnak. És arra való, hogy 
+
+        //függvény kell. Ha ide hozzuk léte akkor, példány.tulajdonsággal kell létrehozni
+        public void Roll()
+        {
+            D6 = random.Next(1, 7);  //a második szám már nincs benne, tehát 1-6 közötti számok
+
+        }
+
+        public int GetCurrent( Dice dice )
+        {
+            return dice.D6;
+        }
+
+        public void Reroll()
+        {
+            if (D6 != 0)
+            {
+                reD6 = random.Next(1, 7);
+            }
+
+        }
+
+        public int Has6()
+        {
+            return;
+        }
+    }
+}
---
+++ week-04/day1/Dice/Dice/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dice;
+
+namespace Dice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*objektum létrehozása, ha csak egy dobás kellene
+            Dice Dice1= new Dice();*/
+
+            List<Dice> dices = new List<Dice>();
+
+            for (int i = 0; i < 6; i++)
+            {
+                dices.Add(new Dice());
+                //foreach-csel lehet kiíratni
+            }
+
+
+            for (int j = 0; j < dices.Count; j++)
+            {
+                dices[j].Roll();
+            }
+
+            Console.WriteLine();
+
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
