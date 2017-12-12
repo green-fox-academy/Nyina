7120c0028ccc1374d7f84cdb4004090f5f6403e8
Nyina <dobinyina@gmail.com>
2017-12-12 17:35:08 +0100

    feat: Garden in progress

---
+++ week-04/day2/Garden_Application/Garden_Application/Flower.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Garden_Application
+{
+    class Flower : Garden
+    {
+        public double flowerWaterAmount;
+        public double number;
+
+        public Flower(double number)
+            {
+                this.number = number;
+            }
+
+        public double flowerWatering()
+        {
+           return flowerWaterAmount = 0.75 * waterAmount;
+        }
+
+    }
+}
---
+++ week-04/day2/Garden_Application/Garden_Application/Garden.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Garden_Application
+{
+    class Garden
+    {
+        public int waterFirst = 40;
+        public int waterSecon = 70;
+        public int waterAmount;
+
+
+        public int GetFirstWater()
+        {
+            return waterAmount += waterFirst;
+        }
+
+        public int GetSecondWater()
+        {
+            return waterAmount += waterFirst;
+        }
+    }
+}
