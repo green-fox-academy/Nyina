dfc8949905cac296bcfb2ea59a7cf5e6b009710f
Nyina <dobinyina@gmail.com>
2017-12-12 08:59:46 +0100

    feat: Counter done

---
+++ week-04/day1/Counter/Counter/Counter.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Counter
+{
+    class Counter
+    {
+        public int num;
+        public int currVal;
+
+        public int CounterNum()
+        {
+            return num = 0;
+        }
+
+        public int CurrentValue()
+        {
+            return currVal = num;
+        }
+
+        public int AddNumber()
+        {
+            return currVal += 1;
+        }
+
+        public int Add()
+        {
+            return currVal++;
+        }
+
+        public string Get()
+        {
+            return currVal.ToString();
+        }
+
+        public int Reset()
+        {
+            return num = 0;
+        }
+
+    }
+}
---
+++ week-04/day1/Counter/Counter/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Counter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Counter numberCounter = new Counter();
+
+            numberCounter.CounterNum();
+            Console.WriteLine(numberCounter.num);
+
+            numberCounter.CurrentValue();
+            Console.WriteLine(numberCounter.currVal);
+
+            numberCounter.AddNumber();
+            Console.WriteLine(numberCounter.currVal);
+
+            numberCounter.Add();
+            Console.WriteLine(numberCounter.currVal);
+
+            numberCounter.Get();
+            Console.WriteLine(numberCounter.currVal);
+
+            numberCounter.Reset();
+            Console.WriteLine(numberCounter.num);
+
+            Console.ReadLine();
+
+        }
+    }
+}
