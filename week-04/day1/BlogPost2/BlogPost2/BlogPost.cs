d145a2d1f11a4f10ab2fa35ea79dffa0057ad285
Nyina <dobinyina@gmail.com>
2017-12-13 22:38:19 +0100

    feat: BlogPost2 practice

---
+++ week-04/day1/BlogPost2/BlogPost2/BlogPost.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogPost2
+{
+    class BlogPost
+    {
+        public string AuthorName;
+        public string Title;
+        public string Text;
+        public string PublicationDate;
+
+        //csak akkor kell a függvénynek paramétert adni, ha azok kívülről jönnek
+        public void Print()
+        {
+            //azért nem kell megadni a classt, mert ott van a funkció, szóval ide tartozik
+            //a this opcionális, a classra utal
+            Console.WriteLine(this.AuthorName);
+            Console.WriteLine(this.Title);
+            Console.WriteLine(this.Text);
+            Console.WriteLine(this.PublicationDate);
+
+        }
+
+        public void PrintToUpper()
+        {
+            Console.WriteLine(this.AuthorName.ToUpper());
+            Console.WriteLine(this.Title.ToUpper());
+            Console.WriteLine(this.Text.ToUpper());
+        }
+
+        public void PrintToUpperOrNot(int number)
+        {
+
+            if (number == 1)
+            {
+            Console.WriteLine(this.AuthorName);
+            Console.WriteLine(this.Title);
+            Console.WriteLine(this.Text);
+            Console.WriteLine(this.PublicationDate);
+            }
+
+            if (number == 2)
+            {
+            Console.WriteLine(this.AuthorName.ToUpper());
+            Console.WriteLine(this.Title.ToUpper());
+            Console.WriteLine(this.Text.ToUpper());
+            }
+        }
+
+    }
+}
---
+++ week-04/day1/BlogPost2/BlogPost2/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogPost2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BlogPost posts0 = new BlogPost();
+
+            posts0.Title = "Lorem Ipsum";
+            posts0.AuthorName = "John Doe";
+            posts0.PublicationDate = "2000.05.04.";
+            posts0.Text = "Lorem ipsum dolor sit amet.";
+
+            //posts0.Print();
+
+            Console.WriteLine();
+
+            //posts0.PrintToUpper();
+
+            Console.WriteLine();
+
+            posts0.PrintToUpperOrNot(1);
+            posts0.PrintToUpperOrNot(2);
+
+            Console.WriteLine();
+            Console.WriteLine();
+
+            BlogPost posts1 = new BlogPost();
+
+            posts1.Title = "Wait but why";
+            posts1.AuthorName = "Tim Urban";
+            posts1.PublicationDate = "2010.10.10.";
+            posts1.Text = "A popular long-form, stick - figure - illustrated blog about almost everything.";
+
+            posts1.Print();
+
+
+            Console.WriteLine();
+            Console.WriteLine();
+
+            BlogPost posts2 = new BlogPost();
+
+            posts2.Title = "One Engineer Is Trying to Get IBM to Reckon With Trump";
+            posts2.AuthorName = "William Turton";
+            posts2.PublicationDate = "2017.03.28.";
+            posts2.Title = "Daniel Hanley, a cybersecurity engineer at IBM, doesn’t want to be the center of attention. When I asked to take his picture outside one of IBM’s New York City offices, he told me that he wasn’t really into the whole organizer profile thing.";
+
+            posts2.Print();
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
