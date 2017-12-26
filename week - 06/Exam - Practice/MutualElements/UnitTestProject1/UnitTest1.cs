72bf299cb7f90821579d02e2ce06d900d389f458
Nyina <dobinyina@gmail.com>
2017-12-26 15:08:42 +0100

    feat: MutualElements 3 tests

---
+++ week - 06/Exam - Practice/MutualElements/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,50 @@
+﻿using NUnit.Framework;
+using MutualElements;
+using System.Collections.Generic;
+
+namespace UnitTestProject1
+{
+    [TestFixture]
+    public class UnitTest1
+    {
+        //Program program = new Program();
+
+        [Test]
+        public void UniqueCharacterList()
+        {
+            List<int> firstList = new List<int> { 1, 1, 2, 3, 3, 4 };
+            List<int> secondList = new List<int>() { 3, 4, 4, 5, 6 };
+            List<int> commonElementsList = new List<int> { 3, 4 };
+
+            //Ha nem lenne static a method, akkor példányosítani kell az osztályt, 
+            //egyébként így lehet elérni a methodot:
+            var result = Program.FilterMutualElements(firstList, secondList);
+
+            Assert.AreEqual(commonElementsList, result);
+        }
+
+        [Test]
+        public void UniqueCharactersFromSameLists()
+        {
+            List<int> firstList = new List<int> { 1, 1, 2, 3, 3, 4 };
+            List<int> secondList = new List<int>() { 1, 1, 2, 3, 3, 4 };
+            List<int> commonElementsList = new List<int> { 1, 2, 3, 4 };
+            var result = Program.FilterMutualElements(firstList, secondList);
+
+            Assert.AreEqual(commonElementsList, result);
+        }
+
+        [Test]
+        public void UniqueCharactersIfThereAreNoCommonElements()
+        {
+            List<int> firstList = new List<int> { 1, 1, 2, 3, 3, 4 };
+            List<int> secondList = new List<int>() { 5, 6, 7 };
+            List<int> commonElementsList = new List<int> { };
+            var result = Program.FilterMutualElements(firstList, secondList);
+
+            Assert.AreEqual(commonElementsList, result);
+        }
+
+
+    }
+}
