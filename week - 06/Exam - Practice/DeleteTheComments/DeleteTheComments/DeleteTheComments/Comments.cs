cb32be69cb70dac41122785f6ba4b2832513859f
Nyina <dobinyina@gmail.com>
2017-12-27 12:08:21 +0100

    feat: DeleteTheComments

---
+++ week - 06/Exam - Practice/DeleteTheComments/DeleteTheComments/DeleteTheComments/Comments.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeleteTheComments
+{
+    class Comments
+    {
+        /* Create a function that takes a c# source file path (e.g. "src.cs") as an input, 
+        reads the given file, and remove the single line comments from it. It should write the comment-free */
+        // code to the original file. (e.g. this line of comment is single lined, the one above not)
+
+    }
+}
---
+++ week - 06/Exam - Practice/DeleteTheComments/DeleteTheComments/DeleteTheComments/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace DeleteTheComments
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DeleteComments("Comments.cs");
+
+            Console.ReadLine();
+
+        }
+        /*
+        public static void DeleteComments(string filename)
+        {
+            string characters = "//";
+
+            string[] lines = File.ReadAllLines(filename);
+
+            foreach (var line in lines)
+            {
+                if (line.Contains(characters))
+                {
+                    line.Remove(0, line.Length);
+                }
+            }
+            File.WriteAllLines(filename, lines);
+            foreach (var line in lines)
+            {
+                Console.WriteLine(line);
+            }
+        }
+        */
+
+
+        public static void DeleteComments(string filename)
+        {
+            string characters = "//";
+
+            string[] lines = File.ReadAllLines(filename);
+            List<string> commentedLines = new List<string>();
+            List<string> outOfCommentedLines = new List<string>();
+
+            foreach (var line in lines)
+            {
+                outOfCommentedLines.Add(line);
+            }
+
+            foreach (var outOfCommentedLine in outOfCommentedLines)
+            {
+                if (outOfCommentedLine.Contains(characters))
+                {
+                    commentedLines.Add(outOfCommentedLine);
+                }
+            }
+
+            foreach (var commentedLine in commentedLines)
+            {
+                outOfCommentedLines.Remove(commentedLine);
+            }
+
+        //Tömbbe is át lehet tölteni az outOfCommentedLines, így outOfCommentedLines.ToArray(),
+        //de listában hagyva is kiírja
+            File.WriteAllLines(filename, outOfCommentedLines.ToArray());
+            foreach (var outOfCommentedLine in outOfCommentedLines)
+            {
+                Console.WriteLine(outOfCommentedLine);
+            }
+        }
+
+    }
+}
