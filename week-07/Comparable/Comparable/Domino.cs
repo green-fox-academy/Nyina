7c6bc4c576a26624c1595ffb674a6bf651d146db
Nyina <dobinyina@gmail.com>
2018-01-02 16:50:51 +0100

    feat: Comperable

---
+++ week-07/Comparable/Comparable/Domino.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Comparable
+{
+    class Domino : IComparable
+    {
+        private readonly int[] Values;
+
+        public int CompareTo(object domino)
+        {
+            Domino otherDomino = domino as Domino;
+            if (this.Values[0] < otherDomino.Values[0])
+            {
+                return -1;
+            }
+            else if (this.Values[0] == otherDomino.Values[0])
+            {
+                return 0;
+            }
+            else
+            {
+                return 1;
+            }
+        }
+
+        public Domino(int valueA, int valueB)
+        {
+            this.Values = new int[] { valueA, valueB };
+        }
+
+        public int[] GetValues()
+        {
+            return Values;
+        }
+    }
+}
