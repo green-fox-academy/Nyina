0c21d68d4ed9bf48ba3c483272d7428138aaa142
Nyina <dobinyina@gmail.com>
2018-01-03 15:08:07 +0100

    feat: Excercise4

---
+++ week-07/day02/Exercise4/Exercise4/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Write a LINQ Expression to find which number squared value is more then 20 from the following array:
+            int[] n = new[] { 3, 9, 2, 8, 6, 5 };
+
+            //var highSquaredNumber = n.Where(num => Math.Pow(num, 2) > 20).ToList();
+
+            var highSquaredNumber = n.Where(num => Math.Pow(num, 2) > 20).Select(x => Math.Pow(x, 2)).ToList();
+
+            highSquaredNumber.ForEach(i => Console.WriteLine($"[{i}]"));
+
+            Console.ReadLine();
+        }
+    }
+}
