db3ffa96ce2488828d3944d60999ea18ab7d6214
Nyina <dobinyina@gmail.com>
2018-01-03 16:13:38 +0100

    feat: Exercise5

---
+++ week-07/day02/Exercise5/Exercise5/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Write a LINQ Expression to find the frequency of numbers in the following array:
+            int[] n = new int[] { 5, 9, 1, 2, 3, 7, 5, 6, 7, 3, 7, 6, 8, 5, 4, 9, 6, 2 };
+
+            var frequencyOfNumbers = n.Select(x => x)
+                                     .OrderByDescending(x => x)
+                                     .GroupBy(k => k)
+                                     .ToDictionary(x => x.Key, x => x.Count());
+
+
+            foreach (int number in frequencyOfNumbers.Keys)
+            {
+                Console.WriteLine("{0}:{1}", number, frequencyOfNumbers[number]);
+            }
+
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
