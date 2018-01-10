d0b87f3af82f2daec50913d7415125d503c3cd67
Nyina <dobinyina@gmail.com>
2018-01-10 17:16:05 +0100

    feat: Bank_of_Simba Part9

--- week-08/day03/Bank_of_Simba/Bank_of_Simba/Controllers/BankAccountController.cs
+++ week-08/day03/Bank_of_Simba/Bank_of_Simba/Controllers/BankAccountController.cs
@@ -8,16 +8,35 @@ using Bank_of_Simba.ViewModel;
 
 namespace Bank_of_Simba.Controllers
 {
-    [Route("Simba")]
+    [Route("")]
     public class BankAccountController : Controller
     {
         //Az osztály 1 példányát hozom létre és ezt adom a View-hoz (lentebb)
         public static BankAccountViewModel bankAccountVM = new BankAccountViewModel();
 
-        [HttpGet("")]
+        [HttpGet("Simba")]
         public IActionResult Index()
         {
             return View(bankAccountVM);
+        }
+
+        [HttpPost("Simba")]
+        public IActionResult AddBalance()
+        {
+            //A view modellen belül van a lista és az in utánival tudok rá hivatkozni
+            //(a fenti példány.BankAccountViewModell-ben szereplő lista neve:
+            foreach (var bankAccListElement in bankAccountVM.BankAccountList)
+            {
+                if (bankAccListElement.Owner.Equals("King"))
+                {
+                    bankAccListElement.Balance += 100;
+                }
+                else
+                {
+                    bankAccListElement.Balance += 10;
+                }
+            }
+            return RedirectToAction("Index");
         }
 
     }
