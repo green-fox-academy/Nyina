08a84203b67da9a87cf048707357ba452ee1e8a1
Nyina <dobinyina@gmail.com>
2018-01-11 12:15:44 +0100

    feat: Counter

---
+++ week-08/day04/Counter/Counter/Controllers/CounterController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Counter.Model;
+
+namespace Counter.Controllers
+{
+    [Route("")]
+    public class CounterController : Controller
+    {
+        private CounterModel counterModel;
+
+        public CounterController (CounterModel counterModel)
+        {
+            this.counterModel = counterModel;
+        }
+
+        [HttpGet("")]
+        public IActionResult Index()
+        {
+            return View(counterModel);
+        }
+
+        [HttpPost("add")]
+        public IActionResult AddBalance()
+        {
+            counterModel.Number += 10;
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ week-08/day04/Counter/Counter/Models/CounterModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Counter.Model
+{
+    public class CounterModel
+    {
+        public int Number { get; set; } = 2;
+    }
+}
