148a86d13adc8a399adc6b6c5e9a06d478eed0d1
Nyina <dobinyina@gmail.com>
2018-01-22 14:11:54 +0100

    feat: greeter

--- week-10/day01/Task/Task/Controllers/HomeController.cs
+++ week-10/day01/Task/Task/Controllers/HomeController.cs
@@ -25,5 +25,15 @@ namespace Task.Controllers
             }
             return Json(new { received = input, result = input * 2 });
         }
+
+        [HttpGet("/greeter")]
+        public IActionResult Greeter(string name, string title)
+        {
+            if (name == null && title == null)
+            {
+                return Json(new { error = "Please provide a name!" });
+            }
+            return Json(new { welcome_message = string.Format("Oh, hi there {0}, my dear {1}!", name, title)});
+            }
     }
 }
